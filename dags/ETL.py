f20cf1e84bbdeb6bd7ba5e8982534d80096d32f5
rosalinathuffi <ochathuffi@gmail.com>
2024-10-09 20:31:06 +0700

    perbaikan folder

--- dags/ETL.py
+++ dags/ETL.py
@@ -11,7 +11,7 @@ from sqlalchemy import create_engine
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../tasks'))
 
 # Mengimpor fungsi extract_data
-from extraction_web import extract_data
+from tasks.extraction_web import extract_data
 
 # Definisi DAG
 @dag(
